4e90d20d581cf8f97d5828abf57faf329f8d4777
Ashley Maceli and Luca Quatela <ashley@>
2016-06-07 11:51:18 -0700

    add ember app skeleton and article model

---
+++ app/routes/index.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+var articles = [{
+  id: 1,
+  title: "Portland Has The Best Ice Cream",
+  author: "John Doe",
+  image: "image",
+  story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}, {
+  id: 2,
+  title: "Bernie Sanders is Cool",
+  author: "Jane Doe",
+  image: "image",
+  story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}, {
+  id: 3,
+  title: "Pizza is Super Good",
+  author: "John Smith",
+  image: "image",
+  story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}];
+
+export default Ember.Route.extend({
+  model() {
+    return articles;
+  },
+});
