bcc8791d5450567481dd9d0f35d094fdf85feef0
Ashley Maceli and Luca Quatela <luca@>
2016-06-07 13:33:13 -0700

    add image and article info in the index, created article-tile component

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,19 +4,19 @@ var articles = [{
   id: 1,
   title: "Portland Has The Best Ice Cream",
   author: "John Doe",
-  image: "image",
+  image: "http://www.americasdairyland.com/assets/images/EWC/Ice-Cream-Hdr.jpg",
   story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }, {
   id: 2,
   title: "Bernie Sanders is Cool",
   author: "Jane Doe",
-  image: "image",
+  image: "http://www.slate.com/content/dam/slate/articles/news_and_politics/politics/2015/07/150706_POL_Sanders.jpg.CROP.promo-xlarge2.jpg",
   story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }, {
   id: 3,
   title: "Pizza is Super Good",
   author: "John Smith",
-  image: "image",
+  image: "http://www.cicis.com/media/1138/pizza_trad_pepperoni.png",
   story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }];
 
